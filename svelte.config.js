db8ffa9fa710dcdccca73e9d91e5689ef4f9adce
Joseph Bravo <josephangelobravo@gmail.com>
2022-05-01 18:43:50 +0000

    initial commit

---
+++ svelte.config.js
@@ -0,0 +1,20 @@
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs'
+		})
+	},
+
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	]
+};
+
+export default config;
