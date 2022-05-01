88e4784387c67ce265fb44d2f7cea7dd0d4a1cd4
Joseph Bravo <josephangelobravo@gmail.com>
2022-05-01 19:02:22 +0000

    added pathing

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,23 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
+//? Make sure to edit this!
+
+const yourRepoName = 'svelte-tailwind-ghpages-template';
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({ pages: 'build', assets: 'build' }),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build'
+		}),
 		prerender: {
 			default: true
+		},
+		paths: {
+			base: dev ? '' : '/' + yourRepoName
 		}
 	},
 	preprocess: [
